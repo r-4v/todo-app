4b8f8b94ee78c54147573342adc835efd7890e13
r-4v <pravaspraveen777@gmail.com>
2021-10-22 04:06:40 +0530

    ui design progress v2

--- src/modules/dom/task-info.js
+++ src/modules/dom/task-info.js
@@ -1,6 +1,18 @@
 const taskInfo = (function(){
     let taskInfoDiv = document.createElement('div');
     taskInfoDiv.setAttribute('style','height:720px;width:440px;background-color:#161616;margin-top:120px;margin-left:40px;border-radius:25px;');
+    let taskHeaderDiv = document.createElement('div');
+    taskHeaderDiv.setAttribute('style',`width:440px;background-color:#252525;height:60px;border-radius:25px 25px 0px 0px;display:flex;justify-content:space-between;`)
+    let taskDetailsHeaderDiv = document.createElement('div');
+    taskDetailsHeaderDiv.setAttribute('style',`color:white;padding:20px;`);
+    taskDetailsHeaderDiv.innerText = "TASK DETAILS";
+    let markDoneBtn = document.createElement('div');
+    markDoneBtn.setAttribute('style',`color:#76ff03;padding:20px;cursor:pointer;`);
+    markDoneBtn.innerText = "Mark as Done";
+
+    taskHeaderDiv.appendChild(taskDetailsHeaderDiv);
+    taskHeaderDiv.appendChild(markDoneBtn);
+    taskInfoDiv.appendChild(taskHeaderDiv);
     return {taskInfoDiv}
 })()
 export {taskInfo};
--- src/modules/dom/task-list.js
+++ src/modules/dom/task-list.js
@@ -1,6 +1,28 @@
-const taskList = (function(){
-    let taskListDiv = document.createElement('div');
-    taskListDiv.setAttribute('style','height:720px;width:440px;background-color:#161616;margin-top:120px;margin-left:40px;border-radius:25px;');
-    return {taskListDiv}
-})()
-export {taskList};
+const taskList = (function () {
+  let taskListDiv = document.createElement("div");
+  taskListDiv.setAttribute(
+    "style",
+    `height:720px;width:440px;background-color:#161616;
+    margin-top:120px;margin-left:40px;border-radius:25px;`
+  );
+  let nameEntryDiv = document.createElement("input");
+  nameEntryDiv.setAttribute(
+    "style",
+    `height:40px;padding:10px;background-color:#414141;
+    width:355px;border-radius:50px;position:relative;
+    left:10px;top:660px;border:none;`
+  );
+  let addEntryDiv = document.createElement("div");
+  addEntryDiv.setAttribute(
+    "style",
+    `height:40px;padding:10px;background-color:#414141;
+    width:40px;position:relative;left:375px;
+    top:620px;border-radius:20px;border:none;`
+  );
+
+  taskListDiv.append(nameEntryDiv);
+  taskListDiv.appendChild(addEntryDiv);
+
+  return { taskListDiv };
+})();
+export { taskList };
