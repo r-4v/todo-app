a844403b183877c4b9a51fcf96f75a6ab53a7e77
r-4v <pravaspraveen777@gmail.com>
2021-10-23 15:21:07 +0530

    added priority options in the ui

--- src/modules/dom/task-info.js
+++ src/modules/dom/task-info.js
@@ -4,11 +4,16 @@ import './task-detail-area.css';
 import { textAreaTrim } from './text-area-trimmer';
 const taskInfo = (function(){
     let taskInfoDiv = document.createElement('div');
-    taskInfoDiv.setAttribute('style','height:720px;width:440px;background-color:#161616;margin-top:120px;margin-left:40px;border-radius:25px;display:flex;flex-direction:column;overflow:scroll;overflow-x:hidden;');
+    taskInfoDiv.setAttribute('style',`height:720px;width:440px;
+    background-color:#161616;margin-top:120px;margin-left:40px;
+    border-radius:25px;display:flex;flex-direction:column;
+    overflow:scroll;overflow-x:hidden;`);
     taskInfoDiv.setAttribute('id','task-info-div');
     taskInfoDiv.classList.add("scroll");
     let taskHeaderDiv = document.createElement('div');
-    taskHeaderDiv.setAttribute('style',`width:440px;background-color:#252525;height:60px;border-radius:25px 25px 0px 0px;display:flex;justify-content:space-between;`)
+    taskHeaderDiv.setAttribute('style',`width:440px;background-color:#252525;
+    height:60px;border-radius:25px 25px 0px 0px;
+    display:flex;justify-content:space-between;`)
 
     let taskDetailsHeaderDiv = document.createElement('div');
     taskDetailsHeaderDiv.setAttribute('style',`color:white;padding:20px;`);
@@ -22,7 +27,7 @@ const taskInfo = (function(){
     taskDetailArea.setAttribute('style','flex-grow:1;border-radius:25px;display:flex;');
     let taskNameDiv = document.createElement('div');
     taskNameDiv.setAttribute('style',`width:90%; margin-left:10px;margin-top:10px;`);
-    taskNameDiv.innerHTML =taskDetails ;
+    taskNameDiv.innerHTML = taskDetails ;
     let selectedDiv = taskNameDiv.getElementsByTagName("textarea");
     // console.log([...selectedDiv][0].value);
     textAreaTrim(selectedDiv);
