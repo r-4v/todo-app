da8f0f6722399521e496e81d9191464f34f28e4e
r-4v <pravaspraveen777@gmail.com>
2021-10-29 07:08:12 +0530

    project list scroll feature

--- src/modules/dom/project-list.js
+++ src/modules/dom/project-list.js
@@ -2,8 +2,12 @@ import {projectList} from "../appLogic/create-project-logic";
 import { todoList } from "../appLogic/create-task-logic";
 import { taskInfo } from "./task-info";
 import {taskList} from "./task-list.js";
+import "./project-list.css";
 let projectsListDiv = document.createElement('div');
 projectsListDiv.setAttribute('id',"projects-list");
+projectsListDiv.setAttribute('style',`overflow:scroll;overflow-x:hidden;`)
+
+
 
 function addProjectListItem(projectName){
     projectsListDiv.innerHTML="";
@@ -11,7 +15,7 @@ function addProjectListItem(projectName){
       //console.log("henlo");
       let projectListItem = document.createElement('div');
       projectListItem.innerText = project.projectName;
-      projectListItem.setAttribute('style',`display:flex;color:white;font-weight:bold;font-size:18px;cursor:pointer;margin:10px;justify-content:center;align-items:center;background:black;padding:10px;border-radius:10px;`);
+      projectListItem.setAttribute('style',`display:flex;color:white;font-weight:bold;font-size:18px;cursor:pointer;margin:10px;justify-content:center;align-items:center;background:black;padding:10px;border-radius:10px; `);
       projectsListDiv.appendChild(projectListItem);
       projectListItem.addEventListener('click',displayProjectTasks);
 });
